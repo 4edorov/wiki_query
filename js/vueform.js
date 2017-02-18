0fad65c1a5ec83268c65295a854360d9e438ab01
4edorov <4edorov@gmail.com>
2017-02-18 07:11:34 +0300

    add button random search

--- js/vueform.js
+++ js/vueform.js
@@ -1,20 +1,18 @@
 Vue.component('random-search', {
   template: `
-    <button v-on:click="randomSearch" type="button" class="btn btn-default"><i class="fa fa-random"></i> Random Search</button>
-  `
-});
-
-let randomSearch = new Vue({
-  el: '#randomSearch',
-  methods: {
-    randomSearch: function(event) {
-      event.preventDefault();
-      this.$http.get(
-      {
-        this.response = data;
-      }).error(function (data, status, request) {
-
-      });
+    <div class="container">
+      <a :href="randomWikiArticle">
+        <button type="button" class="btn btn-default"><i class="fa fa-random"></i> Random Search</button>
+      </a>  
+    </div>
+  `,
+  props: ['randomWikiArticle'],
+  computed: {
+    randomWikiArticle() {
+      return 'https://en.wikipedia.org/wiki/Special:Random'
     }
   }
+});
+let randomSearch = new Vue({
+  el: '#randomSearch',
 });
